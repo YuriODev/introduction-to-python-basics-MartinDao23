b414e8a3852d834aab344b4075feb2379324a29b
MartinDao23 <161600728+MartinDao23@users.noreply.github.com>
2024-02-28 13:25:37 +0000

    Exercise 1

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,5 +1,10 @@
 # Exercise 1
 number = int(input("Enter a five-digit number: "))
-first_part = (number // 10000) + ((number // 100) % 10) + (number % 10)
-second_part = ((number // 1000) % 10) + ((number // 10) % 10)
-print(str(first_part) + str(second_part))
+first_part = (number // 10000)
+third_part = (number // 100) % 10
+fifth_part = (number % 10)
+second_part = (number // 1000) % 10
+fourth_part = (number // 10) % 10
+number_one = first_part + third_part + fifth_part
+number_two = second_part + fourth_part
+print(str(number_one) + str(number_two))
