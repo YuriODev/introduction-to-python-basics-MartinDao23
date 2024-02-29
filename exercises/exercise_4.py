96b1d2b1933c65a45f701ceae3743ea6f9a268ae
MartinDao23 <161600728+MartinDao23@users.noreply.github.com>
2024-02-29 10:47:58 +0000

    Exercise

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,8 @@
 # Exercise 4
-# Your solution comes here
+four_digit = input("Give us a four digit number: ")
+for i in range (4-(len(four_digit))):
+    four_digit = "0" + four_digit
+if four_digit[0] == four_digit[3] and four_digit[1] == four_digit[2]:
+    print(1)
+else:
+    print(0)
