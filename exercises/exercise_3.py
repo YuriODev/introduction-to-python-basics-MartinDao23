2836f0e44d2cb8c5c9e2bb8c144f1deaea70c104
MartinDao23 <161600728+MartinDao23@users.noreply.github.com>
2024-02-29 10:23:13 +0000

    Exercise 3

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,2 +1,7 @@
 # Exercise 3
-# Your solution comes here
+time = int(input("How many seconds have passed since the beginning of the day: "))
+minutes = time // 60
+spare_seconds = time - (minutes*60)
+hours = minutes // 60
+spare_minutes = minutes - (hours*60)
+print(str(hours) + ":" +  str(spare_minutes) + ":" + str(spare_seconds))
