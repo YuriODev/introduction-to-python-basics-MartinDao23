46f72a051356dbe943ee8d91480f2aa5905a4478
MartinDao23 <161600728+MartinDao23@users.noreply.github.com>
2024-03-04 21:34:26 +0000

    exercises

--- exercises/exercise_11.py
+++ exercises/exercise_11.py
@@ -1,2 +1,14 @@
 # Exercise 11
-# Your solution comes here
+s = int(input("How much does your goods cost?  "))
+five_hundreds = s//500
+s = s%500
+hundreds = s//100
+s = s%100
+tens = s//10
+s = s%10
+fives = s//5
+s = s%5
+twos = s//2
+s = s%2
+ones = s
+print(f"{five_hundreds}(500), {hundreds}(100), {tens}(10), {fives}(5),{twos}(2), {ones}(1) ")
